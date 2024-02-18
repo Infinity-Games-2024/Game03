659f19ecc6278635e094e3227568ed7afa71e180
JennyTran17 <teacupjt17@gmail.com>
2024-02-18 19:51:37 +0000

    menu and buttons
    i add home button and main menu

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,12 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CameraController : MonoBehaviour
 {
     // Start is called before the first frame update
     [SerializeField] private Transform player;
 
+    public void GoHome()
+    {
+        SceneManager.LoadSceneAsync("MainMenu");
+    }
     // Update is called once per frame
     private void Update()
     {
