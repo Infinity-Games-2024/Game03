6499275a05022d0b2a8c0a3d1cc45a7982f50b4b
Ken Shao <zhoufanxiu1997@gmail.com>
2024-05-07 20:45:27 +0100

    Some changes

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -13,13 +13,11 @@ public class MainMenu : MonoBehaviour
         {
             Application.Quit();
         }
-    
     // Start is called before the first frame update
     void Start()
     {
         
     }
-
     // Update is called once per frame
     void Update()
     {
