5ba73b6d4c05ccd85ae133396748e07f429d2465
Ken Yuzhou Shao <zhoufanxiu1997@gmail.com>
2024-06-18 19:03:38 +0100

    Revert "fixedDeltaTimeUpdated"
    This reverts commit 30b5ac7d8b49858c98a00545e4c6f62977465a13.

--- Assets/Scripts/PlayerControl/PlayerMovementsSrinivas/PlayerPhoneMovement.cs
+++ Assets/Scripts/PlayerControl/PlayerMovementsSrinivas/PlayerPhoneMovement.cs
@@ -100,7 +100,7 @@ public class PlayerPhoneMovement : MonoBehaviour
                 dirX = 0f;
             }
         }
-        rb.velocity = new Vector2(dirX * moveSpeed*Time.fixedDeltaTime, rb.velocity.y);
+        rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y);
     }
 
     private void HandleJump()
@@ -110,13 +110,13 @@ public class PlayerPhoneMovement : MonoBehaviour
         {
 
             jumpSoundEffect.Play();
-            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            rb.velocity = new Vector2(rb.velocity.x*Time.fixedDeltaTime, jumpForce);
             //isButtonPressed = false;
         }
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
         HandleMovement();
         HandleJump();
