f7547f2b191466e133b5c74ebc7d5cc4d5b64b4f
Ken Shao <zhoufanxiu1997@gmail.com>
2024-07-10 21:10:43 +0100

    More Infinite BG

--- Assets/Scripts/GDTitan/PlayerManager.cs
+++ Assets/Scripts/GDTitan/PlayerManager.cs
@@ -1,19 +1,22 @@
-using System.Collections;
-using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 using TMPro;
 using Cinemachine;
 
 public class PlayerManager : MonoBehaviour
 {
+    public static bool isGameOver;
+
     public static Vector2 lastCheckPointPos = new Vector2(-3, 0);
 
     public CinemachineVirtualCamera VCam;
-    public GameObject[] playerPrefabs;
+    public GameObject gameOverScreen;
+    //public GameObject[] playerPrefabs;
     int characterIndex;
 
     private void Awake()
     {
+        isGameOver = false;
         //Below 4 lines were commented in 28th June Friday
         //characterIndex = PlayerPrefs.GetInt("SelectedCharacter",0);
         //GameObject player = Instantiate(playerPrefabs[characterIndex],lastCheckPointPos, Quaternion.identity);
@@ -25,6 +28,14 @@ public class PlayerManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(isGameOver)
+        {
+            gameOverScreen.SetActive(true);
+        }
+    }
+
+    public void ReplayLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
